846c725e8578d98f959faac059a69f3f4b4ca849
Peter Bowen <pzb@amazon.com>
2016-07-10 20:44:40 +0000

    Update STL parser based CTL spec

--- microsoft/parse-stl.rb
+++ microsoft/parse-stl.rb
@@ -82,7 +82,7 @@ def validEV(asn1)
 		ev_infos << inf[:oid]
 	end
 	ev_infos
-end 
+end
 
 def validExtra(asn1)
         extras = []
@@ -100,69 +100,202 @@ def validExtra(asn1)
         extras
 end
 
-class ContentInfo
-	attr_reader :oid
-	attr_reader :content
-	def initialize(asn1)
-		if not asn1.is_a? OpenSSL::ASN1::Sequence
-			raise "Error A: #{asn1.class}"
-		end
-		if asn1.count != 2
-			raise "Error B: #{asn1.count}"
-		end
-		seq = asn1.take(2)
-		if not seq[0].is_a? OpenSSL::ASN1::ObjectId
-			raise "Error C: #{seq[0].class}"
-		end
-		@oid = seq[0].value
-		if not seq[1].is_a? OpenSSL::ASN1::ASN1Data
-			raise "Error D: #{seq[1].class}"
-		end
-		if not seq[1].value.is_a? Array
-			raise "Error E: #{seq[1].value.class}"
-		end
-		if seq[1].value.count != 1
-			raise "Error F: #{seq[1].value.count}"
-		end
-		@content = seq[1].value[0]
-	end
+module PKCS7
+  # https://tools.ietf.org/html/rfc2315#section-7
+  class ContentInfo
+    attr_reader :contentType
+    attr_reader :content
+    def initialize(asn1)
+      if not asn1.is_a? OpenSSL::ASN1::Sequence
+        raise "Error Aci: #{asn1.class}"
+      end
+      if asn1.count < 1 || asn1.count > 2
+        raise "Error B: #{asn1.count}"
+      end
+      seq = asn1.take(2)
+      if not seq[0].is_a? OpenSSL::ASN1::ObjectId
+        raise "Error C: #{seq[0].class}"
+      end
+      @contentType = seq[0].value
+      @content = nil
+      if asn1.count == 1 # No content
+        return
+      end
+
+      # content is explicitly tagged
+      if not seq[1].class == OpenSSL::ASN1::ASN1Data
+        raise "Error Dx: #{seq[1].class}"
+      end
+      if not seq[1].tag == 0
+        raise "Error E: #{seq[1].tag}"
+      end
+      @content = seq[1].value[0]
+    end
+  end
+
+  # https://tools.ietf.org/html/rfc2315#section-9.1
+  class SignedData
+    attr_reader :digestAlgorithms
+    attr_reader :contentInfo
+    attr_reader :certificates
+    attr_reader :crls
+    attr_reader :signerInfos
+
+    def initialize(asn1)
+      if not asn1.is_a? OpenSSL::ASN1::Sequence
+        raise "Error A: #{asn1.class}"
+      end
+      if asn1.count < 4 || asn1.count > 6
+        raise "Error B: #{asn1.count}"
+      end
+      seq = asn1.take(6)
+      if !seq[0].is_a?(OpenSSL::ASN1::Integer) || seq[0].value != 1
+        raise "Error C: #{seq[0].value}"
+      end
+      seq.shift # Version
+      @signerInfos = seq.pop
+      @digestAlgorithms = seq.shift
+      @contentInfo = seq.shift
+      @certificates = nil
+      @crls = nil
+
+      seq.each do |i|
+        if !i.is_a?(OpenSSL::ASN1::ASN1Data)
+          raise "Error D: #{i.class}"
+        end
+        case i.tag
+        when 0
+          @certificates = i.value
+        when 1
+          @crl = i.value
+        else
+          raise "Error E: #{i.tag}"
+        end
+      end
+    end
+  end
+end
+
+module CTL
+  class TrustedSubject
+    attr_reader :hash
+    attr_reader :attributes
+
+    def initialize(asn1)
+      if not asn1.is_a? OpenSSL::ASN1::Sequence
+        raise "Error A: #{asn1.class}"
+      end
+      if asn1.count < 1 || asn1.count > 2
+        raise "Error B: #{asn1.count}"
+      end
+      seq = asn1.take(2)
+      @attributes = {}
+		  @hash = seq[0].value.force_encoding('BINARY').bytes.map{|c| "%02x" % c}.join('')
+      if seq.count > 1
+        seq[1].value.each do |attr|
+          if !attr.is_a?(OpenSSL::ASN1::Sequence) || attr.count != 2
+            raise "Error TS1: #{attr}"
+          end
+          iseq = attr.take(2)
+          if !iseq[0].is_a?(OpenSSL::ASN1::ObjectId)
+            raise "Error TS2: #{iseq[0]}"
+          end
+          oid = iseq[0].oid
+          if @attributes.key?(oid)
+            raise "Error TS3: #{oid}"
+          end
+          if !iseq[1].is_a?(OpenSSL::ASN1::Set) || iseq[1].count != 1
+            raise "Error TS4: #{iseq[1]}"
+          end
+          @attributes[oid] = iseq[1].take(1).first.value
+        end
+      end
+    end
+  end
+
+  class CertificateTrustList
+    attr_reader :subjectUsage
+    attr_reader :listIdentifier # OPTIONAL
+    attr_reader :sequenceNumber
+    attr_reader :thisUpdate
+    attr_reader :subjectAlgorithm
+    attr_reader :trustedSubjects
+
+    def initialize(asn1)
+      if not asn1.is_a? OpenSSL::ASN1::Sequence
+        raise "Error A: #{asn1.class}"
+      end
+      if asn1.count < 5 || asn1.count > 6
+        raise "Error B: #{asn1.count}"
+      end
+      @listIdentifier = nil
+      seq = asn1.take(6)
+      @subjectUsage = seq.shift
+      if !@subjectUsage.is_a?(OpenSSL::ASN1::Sequence) || @subjectUsage.count != 1
+        raise "Error Z1: #{@subjectUsage}"
+      end
+      @subjectUsage = @subjectUsage.take(1).first.value
+      if (seq.count == 5)
+        @listIdentifier = seq.shift
+        if !@listIdentifier.is_a?(OpenSSL::ASN1::OctetString)
+          raise "Error Z2: #{@listIdentifier}"
+        end
+        @listIdentifier = @listIdentifier.value.force_encoding("UTF-16LE").encode("UTF-8").chomp("\0")
+      end
+      @sequenceNumber = seq.shift
+      if !@sequenceNumber.is_a?(OpenSSL::ASN1::Integer)
+        raise "Error Z3: #{@sequenceNumber}"
+      end
+      @sequenceNumber = @sequenceNumber.value
+      @thisUpdate = seq.shift
+      if !@thisUpdate.is_a?(OpenSSL::ASN1::UTCTime)
+        raise "Error Z4: #{@thisUpdate}"
+      end
+      @thisUpdate = @thisUpdate.value
+      @subjectAlgorithm = seq.shift
+      @trustedSubjects = seq.shift
+    end
+  end
 end
 
 def parseSTL(asn1)
-	p7 = ContentInfo.new(asn1)
-	signed_data = p7.content.take(6)
-	#0: version, 1: digestAlgorithms, 2: ContentInfo, 3: Certs (opt), 4: CRLs (opt), 5: signerinfo
-	content_info = ContentInfo.new(signed_data[2])
-	#0: Seq, 1:, Int 2: Time, 3: Seq, 4: Seq
-	#4 is the one we want
-	stl = content_info.content.take(5)
-	entries = stl[4]
+	p7 = PKCS7::ContentInfo.new(asn1)
+  if p7.contentType != "pkcs7-signedData"
+    raise "Error XI: #{p7.contentType}"
+  end
+	signed_data = PKCS7::SignedData.new(p7.content)
+  signed_content= PKCS7::ContentInfo.new(signed_data.contentInfo)
+  if signed_content.contentType != "1.3.6.1.4.1.311.10.1" # szOID_CTL
+    raise "Error XII: #{p7.contentType}"
+  end
+
+  ctl = CTL::CertificateTrustList.new(signed_content.content)
+
+  # Make sure this is a Root List
+  if ctl.subjectUsage != "1.3.6.1.4.1.311.10.3.9" # szOID_ROOT_LIST_SIGNER
+#    raise "Error YII: #{i.oid}"
+  end
+
 	cert_pointers = {}
-	entries.each do |entry|
-		e1 = entry.take(2)
-		sha1 = e1[0].value.force_encoding('BINARY').bytes.map{|c| "%02x" % c}.join('')
+	ctl.trustedSubjects.each do |entry|
+    ts = CTL::TrustedSubject.new(entry)
+		sha1 = ts.hash
 		if cert_pointers.has_key? sha1
 			raise "Error CC: #{sha1}"
 		end
-		e2 =  e1[1].take(6)
 		pointer = {}
-		e2.each do |e3|
-			e4 = ContentInfo.new(e3)
-			if not e4.content.is_a? OpenSSL::ASN1::OctetString
-				raise "Error AA: #{e4.content.class}"
+		ts.attributes.each do |oid, val|
+			if oid[0..21] != "1.3.6.1.4.1.311.10.11."
+				raise "Error CC: #{oid}"
 			end
-			if e4.oid[0..21] != "1.3.6.1.4.1.311.10.11."
-				raise "Error CC: #{e4.oid}"
-			end
-			prop = e4.oid.split('.').last.to_i
+			prop = oid.split('.').last.to_i
 			if PROPS.has_key? prop
 				prop = PROPS[prop]
 			end
-			if pointer.has_key? prop 
+			if pointer.has_key? prop
 				raise "Error BB: #{prop}"
 			end
-			val = e4.content.value
-			if prop == :FRIENDLY_NAME 
+			if prop == :FRIENDLY_NAME
 				val = FriendlyName(val)
 			elsif prop == :ENHKEY_USAGE
 				val = validExtra(OpenSSL::ASN1.decode(val))
