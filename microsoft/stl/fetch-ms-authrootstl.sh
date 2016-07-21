d8f8f1f8b9718677a3c9d3f2c12a08053849b57a
Peter Bowen <pzb@amazon.com>
2016-07-21 16:29:52 +0000

    Fetch the sst for distrusted certs

--- microsoft/stl/fetch-ms-authrootstl.sh
+++ microsoft/stl/fetch-ms-authrootstl.sh
@@ -3,3 +3,4 @@ curl -O http://ctldl.windowsupdate.com/msdownload/update/v3/static/trustedr/en/a
 cabextract authrootstl.cab
 curl -O http://ctldl.windowsupdate.com/msdownload/update/v3/static/trustedr/en/disallowedcertstl.cab
 cabextract disallowedcertstl.cab
+curl -O http://ctldl.windowsupdate.com/msdownload/update/v3/static/trustedr/en/disallowedcert.sst
