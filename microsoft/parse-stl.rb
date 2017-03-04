7244dd00aa8547669570ab1cefe68381fdbc76b2
Peter Bowen <pzb@amazon.com>
2017-03-04 13:52:32 -0800

    Update STL parser to handle broader options and different ruby versions

--- microsoft/parse-stl.rb
+++ microsoft/parse-stl.rb
@@ -12,87 +12,87 @@ PROPS = {
 98 => :AUTH_ROOT_SHA256_HASH,
 104 => :DISALLOWED_FILETIME,
 105 => :ROOT_PROGRAM_CHAIN_POLICIES,
-122 => :unknown
+122 => :DISALLOWED_ENHKEY_USAGE
 }
 
 def FriendlyName(str)
-	str = str.force_encoding('UTF-16LE').encode('UTF-8')
-	if str[0] == "\u{200E}"
-		str = str[1..-1]
-	end
-	if str[-1] == "\u{0000}"
-		str = str[0..-2]
-	end
+  str = str.force_encoding('UTF-16LE').encode('UTF-8')
+  if str[0] == "\u{200E}"
+    str = str[1..-1]
+  end
+  if str[-1] == "\u{0000}"
+    str = str[0..-2]
+  end
 end
 
 def validEV(asn1)
-	ev_infos = []
-	if not asn1.is_a? OpenSSL::ASN1::Sequence
-		print "Error A: #{asn1.class}\n"
-		return
-	end
-	asn1.each do |elem|
-		inf = {}
-		if not elem.is_a? OpenSSL::ASN1::Sequence
-			print "Error C: #{elem.class}\n"
-			return
-		end
-		if elem.count != 2
-			print "Error D: #{elem.count}\n"
-			return
-		end
-		x = elem.take(2)
-		if not x[0].is_a? OpenSSL::ASN1::ObjectId
-			print "Error E: #{x[0].class}\n"
-			return
-		end
-		inf[:oid] = x[0].value
-		if not x[1].is_a? OpenSSL::ASN1::Sequence
-			print "Error F: #{x[1].class}\n"
-			return
-		end
-		if x[1].count != 1
-			print "Error G: #{x[1].count}\n"
-			return
-		end
-		if not x[1].first.is_a?	OpenSSL::ASN1::Sequence
-			print "Error H: #{x[1].first.class}\n"
-			return
-		end
-		if x[1].first.count != 2
-			print "Error I: #{x[1].first.count}\n"
-			return
-		end
-		y = x[1].first.take(2)
-		if not y[0].is_a? OpenSSL::ASN1::ObjectId
-			print "Error J: #{y[0].class}\n"
-			return
-		end
-		if y[0].value != "1.3.6.1.4.1.311.60.1.1"
-			print "Error K: #{y[0].value}\n"
-			return
-		end
-		if not y[1].is_a? OpenSSL::ASN1::BitString
-			print "Error L: #{y[1].class}\n"
-			return
-		end
-		if y[1].value.bytes != [192]
-			print "Error M: #{y[1].value}\n"
-		end
-		ev_infos << inf[:oid]
-	end
-	ev_infos
+  ev_infos = []
+  if not asn1.is_a? OpenSSL::ASN1::Sequence
+    print "Error A: #{asn1.class}\n"
+    return
+  end
+  asn1.value.each do |elem|
+    inf = {}
+    if not elem.is_a? OpenSSL::ASN1::Sequence
+      print "Error C: #{elem.class}\n"
+      return
+    end
+    if elem.value.count != 2
+      print "Error D: #{elem.value.count}\n"
+      return
+    end
+    x = elem.value.take(2)
+    if not x[0].is_a? OpenSSL::ASN1::ObjectId
+      print "Error E: #{x[0].class}\n"
+      return
+    end
+    inf[:oid] = x[0].value
+    if not x[1].is_a? OpenSSL::ASN1::Sequence
+      print "Error F: #{x[1].class}\n"
+      return
+    end
+    if x[1].value.count != 1
+      print "Error G: #{x[1].value.count}\n"
+      return
+    end
+    if not x[1].value.first.is_a? OpenSSL::ASN1::Sequence
+      print "Error H: #{x[1].value.first.class}\n"
+      return
+    end
+    if x[1].value.first.value.count != 2
+      print "Error I: #{x[1].value.first.count}\n"
+      return
+    end
+    y = x[1].value.first.value.take(2)
+    if not y[0].is_a? OpenSSL::ASN1::ObjectId
+      print "Error J: #{y[0].class}\n"
+      return
+    end
+    if y[0].value != "1.3.6.1.4.1.311.60.1.1"
+      print "Error K: #{y[0].value}\n"
+      return
+    end
+    if not y[1].is_a? OpenSSL::ASN1::BitString
+      print "Error L: #{y[1].class}\n"
+      return
+    end
+    if y[1].value.bytes != [192]
+      print "Error M: #{y[1].value}\n"
+    end
+    ev_infos << inf[:oid]
+  end
+  ev_infos
 end
 
 def validExtra(asn1)
         extras = []
         if not asn1.is_a? OpenSSL::ASN1::Sequence
-                print "Error A: #{asn1.class}\n"
+                print "Error vA: #{asn1.class}\n"
                 return
         end
-        asn1.each do |elem|
+        asn1.value.each do |elem|
                 if not elem.is_a? OpenSSL::ASN1::ObjectId
-                        print "Error B: #{elem.class}\n"
+                        print "Error vB: #{elem.class}\n"
                         return
                 end
                 extras << elem.value
@@ -109,10 +109,10 @@ module PKCS7
       if not asn1.is_a? OpenSSL::ASN1::Sequence
         raise "Error Aci: #{asn1.class}"
       end
-      if asn1.count < 1 || asn1.count > 2
+      if asn1.value.count < 1 || asn1.value.count > 2
         raise "Error B: #{asn1.count}"
       end
-      seq = asn1.take(2)
+      seq = asn1.value.take(2)
       if not seq[0].is_a? OpenSSL::ASN1::ObjectId
         raise "Error C: #{seq[0].class}"
       end
@@ -143,14 +143,14 @@ module PKCS7
 
     def initialize(asn1)
       if not asn1.is_a? OpenSSL::ASN1::Sequence
-        raise "Error A: #{asn1.class}"
+        raise "Error sdA: #{asn1.class}"
       end
-      if asn1.count < 4 || asn1.count > 6
-        raise "Error B: #{asn1.count}"
+      if asn1.value.count < 4 || asn1.value.count > 6
+        raise "Error sdB: #{asn1.count}"
       end
-      seq = asn1.take(6)
+      seq = asn1.value.take(6)
       if !seq[0].is_a?(OpenSSL::ASN1::Integer) || seq[0].value != 1
-        raise "Error C: #{seq[0].value}"
+        raise "Error sdC: #{seq[0].value}"
       end
       seq.shift # Version
       @signerInfos = seq.pop
@@ -161,7 +161,7 @@ module PKCS7
 
       seq.each do |i|
         if !i.is_a?(OpenSSL::ASN1::ASN1Data)
-          raise "Error D: #{i.class}"
+          raise "Error sdD: #{i.class}"
         end
         case i.tag
         when 0
@@ -169,7 +169,7 @@ module PKCS7
         when 1
           @crl = i.value
         else
-          raise "Error E: #{i.tag}"
+          raise "Error sdE: #{i.tag}"
         end
       end
     end
@@ -183,20 +183,20 @@ module CTL
 
     def initialize(asn1)
       if not asn1.is_a? OpenSSL::ASN1::Sequence
-        raise "Error A: #{asn1.class}"
+        raise "Error tsA: #{asn1.class}"
       end
-      if asn1.count < 1 || asn1.count > 2
-        raise "Error B: #{asn1.count}"
+      if asn1.value.count < 1 || asn1.value.count > 2
+        raise "Error tsB: #{asn1.count}"
       end
-      seq = asn1.take(2)
+      seq = asn1.value.take(2)
       @attributes = {}
-		  @hash = seq[0].value.force_encoding('BINARY').bytes.map{|c| "%02x" % c}.join('')
+      @hash = seq[0].value.force_encoding('BINARY').bytes.map{|c| "%02x" % c}.join('')
       if seq.count > 1
         seq[1].value.each do |attr|
-          if !attr.is_a?(OpenSSL::ASN1::Sequence) || attr.count != 2
+          if !attr.is_a?(OpenSSL::ASN1::Sequence) || attr.value.count != 2
             raise "Error TS1: #{attr}"
           end
-          iseq = attr.take(2)
+          iseq = attr.value.take(2)
           if !iseq[0].is_a?(OpenSSL::ASN1::ObjectId)
             raise "Error TS2: #{iseq[0]}"
           end
@@ -204,33 +204,69 @@ module CTL
           if @attributes.key?(oid)
             raise "Error TS3: #{oid}"
           end
-          if !iseq[1].is_a?(OpenSSL::ASN1::Set) || iseq[1].count != 1
+          if !iseq[1].is_a?(OpenSSL::ASN1::Set) || iseq[1].value.count != 1
             raise "Error TS4: #{iseq[1]}"
           end
-          @attributes[oid] = iseq[1].take(1).first.value
+          @attributes[oid] = iseq[1].value.take(1).first.value
         end
       end
     end
   end
 
+=begin
+CertificateTrustList ::= SEQUENCE {
+  version CTLVersion DEFAULT v1
+  subjectUsage SubjectUsage,
+  listIdentifier ListIdentifier OPTIONAL,
+  sequenceNumber HUGEINTEGER OPTIONAL,
+  ctlThisUpdate ChoiceOfTime,
+  ctlNextUpdate ChoiceOfTime OPTIONAL,
+  subjectAlgorithm AlgorithmIdentifier,
+  trustedSubjects TrustedSubjects OPTIONAL,
+  ctlExtensions [0] EXPLICIT Extensions OPTIONAL
+}
+
+CTLVersion ::= INTEGER {v1(0)}
+
+SubjectUsage ::= EnhancedKeyUsage
+
+ListIdentifier ::= OCTETSTRING
+
+TrustedSubjects ::= SEQUENCE OF TrustedSubject
+
+TrustedSubject ::= SEQUENCE{
+  subjectIdentifier SubjectIdentifier,
+  subjectAttributes Attributes OPTIONAL
+}
+
+SubjectIdentifier ::= OCTETSTRING
+=end
+
   class CertificateTrustList
+    attr_reader :version # DEFAULT v1
     attr_reader :subjectUsage
     attr_reader :listIdentifier # OPTIONAL
-    attr_reader :sequenceNumber
+    attr_reader :sequenceNumber # OPTIONAL
     attr_reader :thisUpdate
+    attr_reader :nextUpdate # OPTIONAL
     attr_reader :subjectAlgorithm
-    attr_reader :trustedSubjects
-    attr_reader :extensions
+    attr_reader :trustedSubjects # OPTIONAL
+    attr_reader :extensions # OPTIONAL
 
     def initialize(asn1)
       if not asn1.is_a? OpenSSL::ASN1::Sequence
-        raise "Error A: #{asn1.class}"
+        raise "Error ctlA: #{asn1.class}"
       end
-      if asn1.count < 5 || asn1.count > 7
-        raise "Error B: #{asn1.count}"
+      if asn1.value.count < 3 || asn1.value.count > 9
+        raise "Error ctlB: #{asn1.count}"
       end
       @listIdentifier = nil
-      seq = asn1.take(7)
+      seq = asn1.value.take(8)
+      if seq[0].is_a?(OpenSSL::ASN1::Integer)
+        @version = seq.shift.value
+      else
+        @version = 0
+      end
       @subjectUsage = validExtra(seq.shift)
 
       if seq[0].is_a?(OpenSSL::ASN1::OctetString)
@@ -238,83 +274,103 @@ module CTL
         @listIdentifier = @listIdentifier.value.force_encoding("UTF-16LE").encode("UTF-8").chomp("\0")
       end
 
-      @sequenceNumber = seq.shift
-      if !@sequenceNumber.is_a?(OpenSSL::ASN1::Integer)
-        raise "Error Z3: #{@sequenceNumber}"
+      if seq[0].is_a?(OpenSSL::ASN1::Integer)
+        @sequenceNumber = seq.shift.value
+      else
+        @sequenceNumber = nil
       end
-      @sequenceNumber = @sequenceNumber.value
+
       @thisUpdate = seq.shift
       if !@thisUpdate.is_a?(OpenSSL::ASN1::UTCTime)
-        raise "Error Z4: #{@thisUpdate}"
+        raise "Error ctlC: #{@thisUpdate}"
       end
       @thisUpdate = @thisUpdate.value
+
+      if seq[0].is_a?(OpenSSL::ASN1::UTCTime)
+        @nextUpdate = seq.shift.value
+      end
+
       @subjectAlgorithm = seq.shift
-      @trustedSubjects = seq.shift
+
+      # this could be either a trustedSubjects or a ctlExtensions
+      if seq[0].is_a?(OpenSSL::ASN1::Sequence)
+        # This is a seq, is first thning an OCTET STRING or Object Id
+        first_thing = seq[0].value[0]
+        if first_thing.value[0].is_a?(OpenSSL::ASN1::OctetString)
+          @trustedSubjects = seq.shift.value
+        end
+      end
+
       @extensions = seq.shift
+
+      if !seq.empty?
+        raise "Error ctlD: Extra elements #{seq}"
+      end
     end
   end
 end
 
 def parseSTL(asn1)
-	p7 = PKCS7::ContentInfo.new(asn1)
+  p7 = PKCS7::ContentInfo.new(asn1)
   if p7.contentType != "pkcs7-signedData"
-    raise "Error XI: #{p7.contentType}"
+    raise "Error pstlA: #{p7.contentType}"
   end
-	signed_data = PKCS7::SignedData.new(p7.content)
+  signed_data = PKCS7::SignedData.new(p7.content)
   signed_content= PKCS7::ContentInfo.new(signed_data.contentInfo)
   if signed_content.contentType != "1.3.6.1.4.1.311.10.1" # szOID_CTL
-    raise "Error XII: #{p7.contentType}"
+    raise "Error pstlB: #{p7.contentType}"
   end
 
   ctl = CTL::CertificateTrustList.new(signed_content.content)
 
   # Make sure this is a Root List
-  if ctl.subjectUsage != "1.3.6.1.4.1.311.10.3.9" # szOID_ROOT_LIST_SIGNER
-#    raise "Error YII: #{i.oid}"
+  if ctl.subjectUsage != ["1.3.6.1.4.1.311.10.3.9"] # szOID_ROOT_LIST_SIGNER
+    raise "Error pstlC: #{ctl.subjectUsage}"
   end
 
-	entries = {}
-	ctl.trustedSubjects.each do |entry|
+  entries = {}
+
+  ctl.trustedSubjects.each do |entry|
     ts = CTL::TrustedSubject.new(entry)
-		if entries.key?(ts.hash)
-			raise "Error CC: #{ts.hash}"
-		end
-		properties = {}
-		ts.attributes.each do |oid, val|
-			if oid[0..21] != "1.3.6.1.4.1.311.10.11."
-				raise "Error CC: #{oid}"
-			end
-			prop = oid.split('.').last.to_i
-			if PROPS.has_key? prop
-				prop = PROPS[prop]
-			end
-			if prop == :FRIENDLY_NAME
-				val = FriendlyName(val)
-			elsif prop == :ENHKEY_USAGE
-				val = validExtra(OpenSSL::ASN1.decode(val))
-			elsif prop == :ROOT_PROGRAM_CHAIN_POLICIES
-				val = validExtra(OpenSSL::ASN1.decode(val))
-			elsif prop == :ROOT_PROGRAM_CERT_POLICIES
-				val = validEV(OpenSSL::ASN1.decode(val))
-			elsif prop == :SUBJECT_NAME_MD5_HASH
-				val = val.each_byte.map { |b| sprintf("%02x", b) }.join
-			elsif prop == :KEY_IDENTIFIER
-				val = val.each_byte.map { |b| sprintf("%02x", b) }.join(":")
-			elsif prop == :AUTH_ROOT_SHA256_HASH
-				val = val.each_byte.map { |b| sprintf("%02x", b) }.join
+    if entries.key?(ts.hash)
+      raise "Error pstlD: #{ts.hash}"
+    end
+    properties = {}
+    ts.attributes.each do |oid, val|
+      if oid[0..21] != "1.3.6.1.4.1.311.10.11."
+        raise "Error pstlE: #{oid}"
+      end
+      prop = oid.split('.').last.to_i
+      if PROPS.has_key? prop
+        prop = PROPS[prop]
+      end
+      if prop == :FRIENDLY_NAME
+        val = FriendlyName(val)
+      elsif prop == :ENHKEY_USAGE
+        val = validExtra(OpenSSL::ASN1.decode(val))
+      elsif prop == :ROOT_PROGRAM_CHAIN_POLICIES
+        val = validExtra(OpenSSL::ASN1.decode(val))
+      elsif prop == :ROOT_PROGRAM_CERT_POLICIES
+        val = validEV(OpenSSL::ASN1.decode(val))
+      elsif prop == :SUBJECT_NAME_MD5_HASH
+        val = val.each_byte.map { |b| sprintf("%02x", b) }.join
+      elsif prop == :KEY_IDENTIFIER
+        val = val.each_byte.map { |b| sprintf("%02x", b) }.join(":")
+      elsif prop == :AUTH_ROOT_SHA256_HASH
+        val = val.each_byte.map { |b| sprintf("%02x", b) }.join
       elsif prop == :DISALLOWED_FILETIME
         wtime = val.unpack('q<').first
         val = Time.at((wtime - 116444736000000000) / 10000000).utc
-      elsif prop == :unknown
-				val = validExtra(OpenSSL::ASN1.decode(val))
-			else
-				raise "Unparsed property #{prop}\n"
-			end
-			properties[prop] = val
-		end
-		entries[ts.hash] = properties
-	end
-	entries
+      elsif prop == :DISALLOWED_ENHKEY_USAGE
+        val = validExtra(OpenSSL::ASN1.decode(val))
+      else
+        raise "Error pstlF: Unparsed property #{prop}\n"
+      end
+      properties[prop] = val
+    end
+    entries[ts.hash] = properties
+  end
+  entries
 end
 
 a = OpenSSL::ASN1.decode(File.read(ARGV[0]))
